10de89c9f52bb5d2847f32ecc5ec66edace7bfd1
MuhammadAbdullahEhsan1 <muhammadabdullahehsan1@gmail.com>
2025-04-10 05:11:39 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// The app starts here
 void main() {
   runApp(const MyApp());
 }
@@ -7,116 +8,107 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, // Hides the debug banner
+      home: Padding(padding: EdgeInsets.all(32.0), child: SquareAnimation()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// This widget contains our moving square and buttons
+class SquareAnimation extends StatefulWidget {
+  const SquareAnimation({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SquareAnimation> createState() => SquareAnimationState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SquareAnimationState extends State<SquareAnimation> {
+  // The size of our red square
+  static const _squareSize = 50.0;
 
-  void _incrementCounter() {
+  // Tracks that square is in center
+  Alignment _alignment = Alignment.center;
+
+  // Boolean for animation
+    bool _animating = false;
+
+  // Makes the square bounce
+  final Curve _curve = Curves.elasticInOut;
+
+  // The square's color
+  final MaterialColor _colors = Colors.red;
+
+  // Moves the square left 
+  void _movingLeft() {
+    // Logic to check if already left or currently moving
+    if (_alignment == Alignment.centerLeft || _animating) return;
+
+    // update the state
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _animating = true;
+      _alignment = Alignment.centerLeft;
+    });
+  }
+
+  // Moves the square right
+  void _movingRight() {
+    // Logic to check if already right or currently moving
+    if (_alignment == Alignment.centerRight || _animating) return;
+
+    // Start moving right
+    setState(() {
+      _animating = true;
+      _alignment = Alignment.centerRight;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return Column(
+      children: [
+        AnimatedAlign(
+          duration: const Duration(seconds: 1), // Movement takes 1 second
+          alignment: _alignment, // Where to move
+          curve: _curve, // Makes it bounce little
+          onEnd: () => setState(() => _animating = false), // Done moving
+          child: Container(
+            width: _squareSize,
+            height: _squareSize,
+            color: _colors,
+          ),
+        ),
+
+        // Some space between square and buttons
+        const SizedBox(height: 16),
+
+        // Our control buttons
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Left button
+            ElevatedButton(
+              onPressed:
+                  // Disable button if already left or moving(null)
+                  _alignment == Alignment.centerLeft || _animating
+                      ? null
+                      : _movingLeft,
+              child: const Text('To Left'),
+            ),
+            // Right button
+            ElevatedButton(
+              onPressed:
+                  // Disable button if already right or moving(null)
+                  _alignment == Alignment.centerRight || _animating
+                      ? null
+                      : _movingRight,
+              child: const Text('To Right'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
